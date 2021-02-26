d1d532b5076495ec9203715d037256ce74130fed
mListek <michal.listex@gmail.com>
2021-02-26 14:32:55 +0100

    created Category

---
+++ src/main/java/com/luv2code/ecommerce/entity/Category.java
@@ -0,0 +1,25 @@
+package com.luv2code.ecommerce.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
